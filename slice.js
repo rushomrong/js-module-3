6dfbc91b01b37e6a81eb34e7ff57b6f1c6a7ac07
rushomrong <rushomarak@gmail.com>
2024-09-02 23:17:18 +0600

    slice or split conditoin

---
+++ slice.js
@@ -0,0 +1,17 @@
+const address = "dhaka";
+const part = address.slice(2, 5);
+console.log(part);
+
+//
+
+// const i = "I am a front end web developer with reactJs.";
+// console.log(i.split(" "));
+
+const i = "Rusho,Rahim,Taraque,Noyon,Reza";
+console.log(i.split(",")); // output will split the name
+
+//Join string
+
+const name = ["Rusho", "Bishal", "Rowdro", "Rakib", "Titu"];
+console.log(name.join());
+console.log(name.join("|"));
